9b40d2a3f5ce6a03076bc34cfaa1e6512737eaea
Macaire Yuen <mackyuen@MacBookPro.WORKGR8U-6AB0>
2025-03-27 09:43:08 -0700

    Cleanup codes. Change variable names. Added comments.

--- Analysis/03-Heatmap.R
+++ Analysis/03-Heatmap.R
@@ -31,60 +31,48 @@ g97_de <- read_delim("Results/Fraser_Fir_g97_20250320.txt",
 de_contigs <- c(g51_de$contig, g85_de$contig, g97_de$contig) |> 
   unique()
 
-
 # Check the number of unique contigs that were differentially expressed
 # across all genotypes
 length(de_contigs)
 # [1] 2794
 
-
-# We need to keep the contig names so we negate the first value
-# needles[1] <- TRUE
-
-# needles_logCPM <- logCPM[needles]
-
+# Filter logCPM for those only differentially expressed
 de_logCPM <- logCPM |> 
   filter(contig %in% de_contigs)
 
 dim(de_logCPM)
 # [1] 2794   23
 
-heatmaply(de_logCPM, draw_cellnote = FALSE, Colv = FALSE, RowSideColors = FALSE)
-
 
 # We will take the column name and extract the genotype and treatment
 # Since this is a tibble, we don't want the contig column
-# mDat <- head(de_logCPM, 500)
 treatment <- colnames(de_logCPM)[-1]
 treatment <- str_sub(treatment, 1, 5) |> 
   unique()
 
-
-# de_logCPM <- de_logCPM |> 
-#   mutate(G51CN = mean(c(G51CN1, G51CN2, G51CN3)), .keep = "unused")
-# 
-# de_logCPM <- de_logCPM |> 
-#   mutate(G51MN = mean(c(G51MN1, G51MN2, G51MN3)), .keep = "unused")
-# 
-# de_logCPM <- de_logCPM |> 
-#   mutate(G85CN = mean(c(G85CN1, G85CN2, G85CN3)), .keep = "unused")
-# 
-# de_logCPM <- de_logCPM |> 
-#   mutate(G51CN = mean(c(G51CN1, G51CN2, G51CN3)), .keep = "unused")
-
-
-tmp <- map(treatment, function(t){
+# Calculate the mean of logCPM by treatment
+mean_de_logCPM <- map(treatment, function(t){
   df <- de_logCPM |> 
     select(starts_with(t))
   rowMeans(df)
 })
 
-names(tmp) <- treatment
+names(mean_de_logCPM) <- treatment
 
-tmp <- as.data.frame(tmp)
+mean_de_logCPM <- as.data.frame(mean_de_logCPM)
 
-dim(tmp)
+# Add back rownames to data frame
+rownames(mean_de_logCPM) <- de_logCPM$contig
 
-heatmaply(tmp, scale = "row", show_dendrogram = c(TRUE, FALSE), cexRow = FALSE, 
+dim(mean_de_logCPM)
+# [1] 2794    6
+
+today <- Sys.Date()
+today <- format(today, "%Y%m%d")
+
+heatmaply(mean_de_logCPM, 
+          scale = "row", 
+          show_dendrogram = c(TRUE, FALSE), 
+          cexRow = FALSE, 
           showticklabels = c(TRUE, FALSE),
-          file = "draft_heatmap_20250324.png")
+          file = str_c("Results/draft_heatmap_", today, ".png"))
